9424fa58ca39f7f92e7fec5d59bc3fe35a74395d
echoiwon <echoiwon@gmail.com>
2019-04-18 18:32:55 -0400

    Add method signatures, comments for patient class

--- src/main/java/edu/brown/cs/group1/patient/Patient.java
+++ src/main/java/edu/brown/cs/group1/patient/Patient.java
@@ -1,5 +1,74 @@
 package edu.brown.cs.group1.patient;
 
+import java.util.List;
+
+import edu.brown.cs.group1.template.Template;
+
+/**
+ * Patient class provides access to each patient's unique id and forms filled
+ * out for patient. Also provides ability to create form instance for patient.
+ * @author wchoi11
+ *
+ */
 public class Patient {
+  private final int patientId;
+  // private boolean isWorking;
+  // TODO: Change to isHospitalized?
+
+  /**
+   * Constructor.
+   * @param patientId
+   *          Unique patient id.
+   */
+  public Patient(int patientId) {
+    this.patientId = patientId;
+  }
+
+  /**
+   * Gets patient's id.
+   * @return Patient's id.
+   */
+  public int getPatientId() {
+    return patientId;
+  }
+
+  /**
+   * Sets patient's id.
+   * @param patientId
+   *          Patient's id.
+   */
+  public void setPatientId(int patientId) {
+    this.patientId = patientId;
+  }
+
+  /**
+   * Returns all completed forms for this patient.
+   * @return List of completed forms for this patient. Empty list if no forms
+   *         have been completed for this patient.
+   */
+  public List<Template> getAllFilledTemplates() {
+
+  }
+
+  /**
+   * Returns blank form to fill out for this patient.
+   * @param formName
+   *          Name/type of form.
+   * @return Blank form of specified type. Throws error if no such type exists.
+   */
+  public Template getTemplate(String formName) {
+
+  }
+
+  /**
+   * Returns completed form of specified type for this patient.
+   * @param formName
+   *          Name/type of form.
+   * @return Completed form of given type. Throws error if no form of this type
+   *         is found for this patient.
+   */
+  public Template getFilledTemplate(String formName) {
+
+  }
 
 }
--- src/main/java/edu/brown/cs/group1/template/Template.java
+++ src/main/java/edu/brown/cs/group1/template/Template.java
@@ -1,5 +1,14 @@
 package edu.brown.cs.group1.template;
 
+/**
+ * Template class provides ability to create new forms and customize fields.
+ * Adds ability to save template as a blank form to the database. Adds ability
+ * to update field values of forms.
+ * @author wchoi11
+ *
+ */
 public class Template {
+  private int templateId;
 
+  // TODO: Add methods for updating template field values.
 }
