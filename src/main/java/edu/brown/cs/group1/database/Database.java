c419c216dc9f85b22de77d3a96f0980f888518c0
Esmeralda Montas <esmeraldamontas@rubens-mbp.devices.brown.edu>
2019-04-17 23:29:46 -0400

    database connection

--- src/main/java/edu/brown/cs/group1/database/Database.java
+++ src/main/java/edu/brown/cs/group1/database/Database.java
@@ -1,4 +1,28 @@
 package edu.brown.cs.group1.database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
+
+  // connection that would query to the database
+  private static Connection dbConn;
+
+    /**
+     * This constructor takes in a path to the db file
+     *
+     * @param db Path to the db file
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+  public Database(String db) throws ClassNotFoundException, SQLException {
+        // Set up a connection and store it in a field
+      Class.forName("org.sqlite.JDBC");
+      String url = "jdbc:sqlite:" + db;
+      dbConn = DriverManager.getConnection(url);
+      Statement stat = dbConn.createStatement();
+      stat.executeUpdate("PRAGMA foreign_keys = ON;");
+      stat.close();
+    }
 }
