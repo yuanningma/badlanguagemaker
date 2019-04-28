3a7f131e741100babe3dcad9d16248f710431c57
Julianne Rudner <juliannerudner@macbook-pro.devices.brown.edu>
2019-04-28 15:04:00 -0400

    need to work on js issues

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,24 +1,25 @@
 $(document).ready(() => {
-console.log("jj");
-  $("#myInput").keyup(event => {
-    var keycode = (e.keyCode ? e.keyCode : e.which);
-    if(keycode=='13'){
-console.log("hi");
-    }
-              // const postParameters = {
-              //   actor2:document.getElementById("actorB").value,
-              //     database:document.getElementById("databaseID").value,
-              // }
-              // $.post("/correct3", postParameters, responseJSON => {
-              //     // TODO: Parse the JSON response into a JavaScript object.
-              //     const responseObject = JSON.parse(responseJSON);
-              //     word=responseObject.actor2;
-              //
-              //   $suggestions2.empty();
-              //           var i;
-              //   for (let i = 0, len = responseObject.suggestions2.length; i < len; i++) {
-              //     $suggestions2.append("<option>"+responseObject.suggestions2[i]+"</option>");
+
+  $('#myInput').on('keypress', (event)=> {
+          if(event.which === 13){
+              console.log("hi");
+
+              const postParameters = {
+                input:document.getElementById("myInput").value
+              }
+
+              $.post("/correct", postParameters, responseJSON => {
+                  // TODO: Parse the JSON response into a JavaScript object.
+                  const responseObject = JSON.parse(responseJSON);
+                  word=responseObject.input;
+
+                $suggestions.empty();
+
+                        var i;
+                for (let i = 0, len = responseObject.suggestions.length; i < len; i++) {
+                  $suggestions.append("<option>"+responseObject.suggestions[i]+"</option>");
   }
   });
+          }
+  });
 });
-}
