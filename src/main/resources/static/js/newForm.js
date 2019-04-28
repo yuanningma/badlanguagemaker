3453f17288cd5bc55aa3933a2c442491360398f0
echoiwon <echoiwon@gmail.com>
2019-04-28 16:14:55 -0400

    Fix post error in createform handler

--- src/main/resources/static/js/newForm.js
+++ src/main/resources/static/js/newForm.js
@@ -6,7 +6,8 @@ $(document).ready(() => {
         $("#last").before("<div class=\"form-group\"><label for=\"field" + count + "\" >Field</label><input id=\"field" + count + "\" type=\"text\" class=\"form-control\" aria-describedby=\"field" + count + "\"  placeholder=\"Enter field name\"></div>");
     });
 
-    $("#newForm").on('click', event => {
+    $("#newForm").on('click', (event) => {
+        console.log("herebef");
 
         // let labels = [];
         // for (let i=1; i<count+1; i++) {
@@ -14,6 +15,8 @@ $(document).ready(() => {
         // }
         // const postParameters = {fields: JSON.stringify(labels)};
         const postParameters = {fields: "field"};
+
+
 
 		$.post("/forms/create", postParameters, responseJSON => {
             // Show message that form was successfully created
