e45e85819aaa46add9219b7bc867402ff8f9a8c6
Esmeralda Montas <esmeraldamontas@rubens-mbp.devices.brown.edu>
2019-05-04 11:52:58 -0400

    Count method up

--- src/main/java/edu/brown/cs/group1/database/FormsDatabase.java
+++ src/main/java/edu/brown/cs/group1/database/FormsDatabase.java
@@ -42,6 +42,7 @@ public class FormsDatabase extends Database {
       String url = "jdbc:sqlite:" + path;
       dbConn = DriverManager.getConnection(url);
       Statement stat = dbConn.createStatement();
+      nextFormId = getCount();
       stat.executeUpdate("PRAGMA foreign_keys = ON;");
       stat.close();
 
@@ -382,7 +383,27 @@ public class FormsDatabase extends Database {
     }
   }
 
+    /**
+     * This method retrieved the count of forms currently in the database.
+     * @return
+     *          a integer representing the number of forms in the database.
+     * @throws SQLException
+     *          thrown when the a SQL Exception is thrown.
+     */
   public Integer getCount() throws SQLException {
-    String q=
+    Integer count = 0;
+    if (dbConn != null) {
+      PreparedStatement prep;
+      String query = "SELECT COUNT(formId) FROM form;";
+      prep = dbConn.prepareStatement(query);
+      ResultSet rs = prep.executeQuery();
+
+      while (rs.next()) {
+        count = rs.getInt(1);
+      }
+      rs.close();
+      prep.close();
+    }
+    return count;
   }
 }
