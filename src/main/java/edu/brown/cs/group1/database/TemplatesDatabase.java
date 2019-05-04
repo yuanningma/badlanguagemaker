abe3dbd8fecbed1b20f08b2961ca26eed4a66681
Julianne Rudner <juliannerudner@macbook-pro.devices.brown.edu>
2019-05-04 12:01:02 -0400

    checkstyle

--- src/main/java/edu/brown/cs/group1/database/TemplatesDatabase.java
+++ src/main/java/edu/brown/cs/group1/database/TemplatesDatabase.java
@@ -196,27 +196,26 @@ public class TemplatesDatabase extends Database {
     return templates;
   }
 
-    /**
-     * This method retrieved the count of forms currently in the database.
-     * @return
-     *          a integer representing the number of forms in the database.
-     * @throws SQLException
-     *          thrown when the a SQL Exception is thrown.
-     */
-    public Integer getCount() throws SQLException {
-      Integer count = 0;
-        if (dbConn != null) {
-            PreparedStatement prep;
-            String query = "SELECT COUNT(templateId) FROM template;";
-            prep = dbConn.prepareStatement(query);
-            ResultSet rs = prep.executeQuery();
+  /**
+   * This method retrieved the count of forms currently in the database.
+   * @return a integer representing the number of forms in the database.
+   * @throws SQLException
+   *           thrown when the a SQL Exception is thrown.
+   */
+  public Integer getCount() throws SQLException {
+    Integer count = 0;
+    if (dbConn != null) {
+      PreparedStatement prep;
+      String query = "SELECT COUNT(templateId) FROM template;";
+      prep = dbConn.prepareStatement(query);
+      ResultSet rs = prep.executeQuery();
 
-            while (rs.next()) {
-                count = rs.getInt(1);
-            }
-            rs.close();
-            prep.close();
-        }
-        return count;
+      while (rs.next()) {
+        count = rs.getInt(1);
+      }
+      rs.close();
+      prep.close();
     }
+    return count;
+  }
 }
