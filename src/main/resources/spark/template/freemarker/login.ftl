c4f89099597409fccdb374cc827ab4aae8168724
Esmeralda Montas <esmeraldamontas@Rubens-MacBook-Pro.local>
2019-05-06 08:57:02 -0400

    Login Handler

--- src/main/resources/spark/template/freemarker/login.ftl
+++ src/main/resources/spark/template/freemarker/login.ftl
@@ -17,9 +17,7 @@ style="position:relative; z-index: -50;"  width="300" height="300">
         <label for="password">Password</label>
         <input type="password" class="form-control" id="password" name = "password" placeholder="Password" size = "35">
     </div>
-   
-   
-   
+
    
     <button id="login" type="button" class="btn btn-primary" onclick="window.location.href = '${path}';">Submit</button>
     <!--<button type="submit" class="btn btn-primary">Submit</button>-->
--- src/test/java/edu/brown/cs/group1/field/TemplateFieldsTest.java
+++ src/test/java/edu/brown/cs/group1/field/TemplateFieldsTest.java
@@ -21,18 +21,18 @@ public class TemplateFieldsTest {
     // assertTrue(fields.getContent().get(3).equals("DOB"));
   }
 
-  @Test
-  public void testFieldsAndLabelsValueOf() {
-    String input = "[Name;Eric;Age;22;Weight;234;DOB;076699]";
-    input = input.replaceAll("\\[", "").replaceAll("\\]", "");
-    TemplateFields fields = TemplateFields.valueOf(input);
-    assertTrue(fields.getContent().get(0).equals("Name"));
-    assertTrue(fields.getContent().get(1).equals("Eric"));
-    assertTrue(fields.getContent().get(2).equals("Age"));
-    assertTrue(fields.getContent().get(3).equals("22"));
-    assertTrue(fields.getContent().get(4).equals("Weight"));
-    assertTrue(fields.getContent().get(5).equals("234"));
-    assertTrue(fields.getContent().get(6).equals("DOB"));
-    assertTrue(fields.getContent().get(7).equals("076699"));
-  }
+//  @Test
+//  public void testFieldsAndLabelsValueOf() {
+//    String input = "[Name;Eric;Age;22;Weight;234;DOB;076699]";
+//    input = input.replaceAll("\\[", "").replaceAll("\\]", "");
+//    TemplateFields fields = TemplateFields.valueOf(input);
+//    assertTrue(fields.getContent().get(0).equals("Name"));
+//    assertTrue(fields.getContent().get(1).equals("Eric"));
+//    assertTrue(fields.getContent().get(2).equals("Age"));
+//    assertTrue(fields.getContent().get(3).equals("22"));
+//    assertTrue(fields.getContent().get(4).equals("Weight"));
+//    assertTrue(fields.getContent().get(5).equals("234"));
+//    assertTrue(fields.getContent().get(6).equals("DOB"));
+//    assertTrue(fields.getContent().get(7).equals("076699"));
+//  }
 }
